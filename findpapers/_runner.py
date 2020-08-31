8f2e26b5cd8d4bab4f2ada4f8d06f615ae796fb9
Jonatas Grosman <jonatasgrosman@gmail.com>
2020-08-31 20:40:19 -0300

    refact: change project structure

--- findpapers/_runner.py
+++
@@ -1,7 +0,0 @@
-from searcher.searcher import Searcher
-import configparser
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-Searcher.run(config)
--- tests/unit/test_search_handler.py
+++ tests/unit/test_search_handler.py
@@ -3,11 +3,11 @@ import json
 import findpapers
 
 
-def test_get():
+def test_run():
 
     os.environ['SCOPUS_API_TOKEN'] = 'api-fake-token'
     os.environ['IEEE_API_TOKEN'] = 'api-fake-token'
-    search = findpapers.get('this AND that', limit_per_database=2)
+    search = findpapers.run('this AND that', limit_per_database=2)
 
     fetched_papers_count = 0
     for paper in search.papers:
