d00c75dc14b0573d8ee7809558292b5a7185c05f
Jonatas Grosman <jonatasgrosman@gmail.com>
2021-01-15 20:28:45 -0300

    doc: add more examples in the search_paul.py

--- samples/search_paul.py
+++ samples/search_paul.py
@@ -2,7 +2,7 @@ import findpapers
 import datetime
 
 search_file = "search_paul.json"
-download_output_dir = "/some/valid/dir"
+output_dir = "/some/valid/dir"
 query = "([artificial intelligence] OR [AI] OR [machine learning] OR [ML] OR [deep learning] OR [DL]) AND [rock] AND ([music] OR [song]) AND NOT [drum*]"
 since = datetime.date(2000, 1, 1)
 until = datetime.date(2020, 12, 31)
@@ -13,13 +13,26 @@ limit = 100
 limit_per_database = 4
 databases = ["acm", "ieee", "scopus"]
 publication_types = ["journal", "conference proceedings"]
+categories = {
+    "Research Type": [
+        "Validation Research", "Evaluation Research", "Solution Proposal", "Philosophical", "Opinion", "Experience"
+    ],
+    "Contribution": [
+        "Metric", "Tool", "Model", "Method"
+    ]
+}
+highlights = ["propose", "achiev", "accuracy", "method", "metric", "result", "limitation", "state of the art"]
+
 findpapers.utils.common_util.logging_initialize(True) #verbose
 
 # search
 findpapers.search(search_file, query, since, until, limit, limit_per_database, databases, publication_types, scopus_api_token, ieee_api_token)
 
 # refine
-findpapers.refine(search_file, show_abstract=True, show_extra_info=True)
+findpapers.refine(search_file, categories, highlights, show_abstract=True, show_extra_info=True)
 
 # download
-findpapers.download(search_file, download_output_dir, only_selected_papers=True, proxy=proxy)
+findpapers.download(search_file, output_dir, only_selected_papers=True, proxy=proxy)
+
+# generate bibtex
+findpapers.generate_bibtex(search_file, output_dir, only_selected_papers=True, add_findpapers_citation=True)
