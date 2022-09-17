118f77d5a2be6648789f9ca24f8f26f316a1f726
ZeroCommits <37379471+ZeroCommits@users.noreply.github.com>
2022-09-17 16:14:51 +0800

    09/17/2022 Disable importlib metadata check. This module is used as a relative import in the main package.

--- findpapers/__init__.py
+++ findpapers/__init__.py
@@ -6,9 +6,10 @@ from findpapers.tools.search_runner_tool import search
 from findpapers.tools.refiner_tool import refine
 from findpapers.tools.downloader_tool import download
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
-__version__ = importlib_metadata.version(__name__)
+## 09/17/2022 Disable importlib metadata check. This module is used as a relative import in the main package.
+## try:
+##     import importlib.metadata as importlib_metadata
+## except ModuleNotFoundError:
+##     import importlib_metadata
+## 
+## __version__ = importlib_metadata.version(__name__)
