751abcc6863cb4599b7fb07e415416cb64451c63
Denis Strizhkin <strdenis02@gmail.com>
2025-07-20 19:35:20 +0300

    bump dependencies and python version

--- findpapers/tools/refiner_tool.py
+++ findpapers/tools/refiner_tool.py
@@ -2,14 +2,15 @@ import inquirer
 import re
 import os
 from typing import Optional, List
-from colorama import Fore, Back, Style, init
-from findpapers.models.search import Search
+from termcolor import colored
 from findpapers.models.paper import Paper
 import findpapers.utils.common_util as common_util
 import findpapers.utils.persistence_util as persistence_util
 
 
-def _print_paper_details(paper: Paper, highlights: List[str], show_abstract: bool, show_extra_info: bool):  # pragma: no cover
+def _print_paper_details(
+    paper: Paper, highlights: List[str], show_abstract: bool, show_extra_info: bool
+):  # pragma: no cover
     """
     Private method used to print on console the paper details
 
@@ -25,66 +26,110 @@ def _print_paper_details(paper: Paper, highlights: List[str], show_abstract: boo
         A flag to indicate if the paper's extra info should be shown or not, by default False
     """
 
-    print(f"{Fore.GREEN}{Style.BRIGHT}Title:{Style.NORMAL} {paper.title}")
-    print(f"{Fore.GREEN}{Style.BRIGHT}Authors:{Style.NORMAL} {' | '.join(paper.authors)}")
+    print(f"{colored('Title:', 'green', attrs=['bold'])} {paper.title}")
+    print(f"{colored('Authors:', 'green', attrs=['bold'])} {' | '.join(paper.authors)}")
     if len(paper.keywords) > 0:
-        print(f"{Fore.GREEN}{Style.BRIGHT}Keywords:{Style.NORMAL} {', '.join(paper.keywords)}")
-    print(f"{Fore.GREEN}{Style.BRIGHT}Publication date:{Style.NORMAL} {paper.publication_date.strftime('%Y-%m-%d')}")
+        print(
+            f"{colored('Keywords:', 'green', attrs=['bold'])} {', '.join(paper.keywords)}"
+        )
+    print(
+        f"{colored('Publication date:', 'green', attrs=['bold'])} {paper.publication_date.strftime('%Y-%m-%d')}"
+    )
 
     print("\n")
 
     if show_abstract:
         abstract = paper.abstract
         for term in highlights:
-            abstract = re.sub(r"({0}+)".format(term), Fore.YELLOW + Style.BRIGHT +
-                              r"\1" + Fore.RESET + Style.NORMAL, abstract, flags=re.IGNORECASE)
+            abstract = re.sub(
+                r"({})".format(
+                    re.escape(term)
+                ),  # Use re.escape(term) for safety with special regex characters
+                colored(r"\1", "yellow", attrs=["bold"]),
+                abstract,
+                flags=re.IGNORECASE,
+            )
         print(abstract)
 
         print("\n")
 
-    if show_extra_info:
-        if paper.comments is not None:
-            print(f"{Style.BRIGHT}Paper comments:{Style.NORMAL} {paper.comments}")
-        if paper.citations is not None:
-            print(f"{Style.BRIGHT}Paper citations:{Style.NORMAL} {paper.citations}")
-        if paper.number_of_pages is not None:
-            print(f"{Style.BRIGHT}Paper number of pages:{Style.NORMAL} {paper.number_of_pages}")
-        if paper.doi is not None:
-            print(f"{Style.BRIGHT}Paper DOI:{Style.NORMAL} {paper.doi}")
-        if paper.databases is not None:
-            print(f"{Style.BRIGHT}Paper found in:{Style.NORMAL} {', '.join(paper.databases)}")
-        if len(paper.urls) > 0:
-            print(f"{Style.BRIGHT}Paper URL:{Style.NORMAL} {list(paper.urls)[0]}")
+        if show_extra_info:
+            if paper.comments is not None:
+                print(f"{colored('Paper comments:', attrs=['bold'])} {paper.comments}")
+            if paper.citations is not None:
+                print(
+                    f"{colored('Paper citations:', attrs=['bold'])} {paper.citations}"
+                )
+            if paper.number_of_pages is not None:
+                print(
+                    f"{colored('Paper number of pages:', attrs=['bold'])} {paper.number_of_pages}"
+                )
+            if paper.doi is not None:
+                print(f"{colored('Paper DOI:', attrs=['bold'])} {paper.doi}")
+            if paper.databases is not None:
+                print(
+                    f"{colored('Paper found in:', attrs=['bold'])} {', '.join(paper.databases)}"
+                )
+            if len(paper.urls) > 0:
+                print(f"{colored('Paper URL:', attrs=['bold'])} {list(paper.urls)[0]}")
 
-        if paper.publication is not None:
-            print(f"{Style.BRIGHT}Publication name:{Style.NORMAL} {paper.publication.title}")
-            print(f"{Style.BRIGHT}Publication is potentially predatory:{Style.NORMAL} {paper.publication.is_potentially_predatory}")
-            if paper.publication.category is not None:
-                print(f"{Style.BRIGHT}Publication category:{Style.NORMAL} {paper.publication.category}")
-            if len(paper.publication.subject_areas) > 0:
-                print(f"{Style.BRIGHT}Publication areas:{Style.NORMAL} {', '.join(paper.publication.subject_areas)}")
-            if paper.publication.isbn is not None:
-                print(f"{Style.BRIGHT}Publication ISBN:{Style.NORMAL} {paper.publication.isbn}")
-            if paper.publication.issn is not None:
-                print(f"{Style.BRIGHT}Publication ISSN:{Style.NORMAL} {paper.publication.issn}")
-            if paper.publication.publisher is not None:
-                print(f"{Style.BRIGHT}Publication publisher:{Style.NORMAL} {paper.publication.publisher}")
-            if paper.publication.cite_score is not None:
-                print(f"{Style.BRIGHT}Publication Cite Score:{Style.NORMAL} {paper.publication.cite_score}")
-            if paper.publication.sjr is not None:
-                print(f"{Style.BRIGHT}Publication SJR:{Style.NORMAL} {paper.publication.sjr}")
-            if paper.publication.snip is not None:
-                print(f"{Style.BRIGHT}Publication SNIP:{Style.NORMAL} {paper.publication.snip}")
+            if paper.publication is not None:
+                print(
+                    f"{colored('Publication name:', attrs=['bold'])} {paper.publication.title}"
+                )
+                print(
+                    f"{colored('Publication is potentially predatory:', attrs=['bold'])} {paper.publication.is_potentially_predatory}"
+                )
+                if paper.publication.category is not None:
+                    print(
+                        f"{colored('Publication category:', attrs=['bold'])} {paper.publication.category}"
+                    )
+                if len(paper.publication.subject_areas) > 0:
+                    print(
+                        f"{colored('Publication areas:', attrs=['bold'])} {', '.join(paper.publication.subject_areas)}"
+                    )
+                if paper.publication.isbn is not None:
+                    print(
+                        f"{colored('Publication ISBN:', attrs=['bold'])} {paper.publication.isbn}"
+                    )
+                if paper.publication.issn is not None:
+                    print(
+                        f"{colored('Publication ISSN:', attrs=['bold'])} {paper.publication.issn}"
+                    )
+                if paper.publication.publisher is not None:
+                    print(
+                        f"{colored('Publication publisher:', attrs=['bold'])} {paper.publication.publisher}"
+                    )
+                if paper.publication.cite_score is not None:
+                    print(
+                        f"{colored('Publication Cite Score:', attrs=['bold'])} {paper.publication.cite_score}"
+                    )
+                if paper.publication.sjr is not None:
+                    print(
+                        f"{colored('Publication SJR:', attrs=['bold'])} {paper.publication.sjr}"
+                    )
+                if paper.publication.snip is not None:
+                    print(
+                        f"{colored('Publication SNIP:', attrs=['bold'])} {paper.publication.snip}"
+                    )
 
         print("\n")
 
-    if paper.selected is not None:
+        if paper.selected is not None:
+            selected_text = "Yes" if paper.selected else "No"
+            print(f"{colored('Selected:', 'blue', attrs=['bold'])} {selected_text}")
 
-        print(f"{Fore.BLUE}{Style.BRIGHT}Selected: {Style.NORMAL}{'Yes' if paper.selected else 'No'}")
-        
-        if paper.categories is not None and len(paper.categories.items()) > 0:
-            categories_string = " | ".join([f"{k}: {', '.join(v)}" for k, v in paper.categories.items() if len(v) > 0])
-            print(f"{Fore.BLUE}{Style.BRIGHT}Categories: {Style.NORMAL}{categories_string}")
+            if paper.categories is not None and len(paper.categories.items()) > 0:
+                categories_string = " | ".join(
+                    [
+                        f"{k}: {', '.join(v)}"
+                        for k, v in paper.categories.items()
+                        if len(v) > 0
+                    ]
+                )
+                print(
+                    f"{colored('Categories:', 'blue', attrs=['bold'])} {categories_string}"
+                )
 
         print("\n")
 
@@ -99,14 +144,11 @@ def _get_select_question_input():  # pragma: no cover
         User provided input
     """
     questions = [
-        inquirer.List("answer",
-                      message="Do you wanna select this paper?",
-                      choices=[
-                          "Skip", 
-                          "No", 
-                          "Yes", 
-                          "Save what I've done so far and leave"],
-                      ),
+        inquirer.List(
+            "answer",
+            message="Do you wanna select this paper?",
+            choices=["Skip", "No", "Yes", "Save what I've done so far and leave"],
+        ),
     ]
     return inquirer.prompt(questions).get("answer")
 
@@ -114,7 +156,7 @@ def _get_select_question_input():  # pragma: no cover
 def _get_category_question_input(categories: dict):  # pragma: no cover
     """
     Private method that prompts a question about the paper category
-    
+
     Parameters
     ----------
     categories : dict
@@ -129,29 +171,37 @@ def _get_category_question_input(categories: dict):  # pragma: no cover
     selections = {}
 
     for facet, facet_categories in categories.items():
-
         questions = [
-            inquirer.Checkbox("answer",
-                        message=f"With respect to \"{facet}\", which categories does the document belong to?",
-                        choices=facet_categories,
-                        ),
+            inquirer.Checkbox(
+                "answer",
+                message=f'With respect to "{facet}", which categories does the document belong to?',
+                choices=facet_categories,
+            ),
         ]
 
         answers = inquirer.prompt(questions)
 
         selections[facet] = answers.get("answer")
-        
+
     return selections
 
 
-def refine(search_path: str, categories: Optional[dict] = None, highlights: Optional[list] = None, show_abstract: Optional[bool] = False, 
-           show_extra_info: Optional[bool] = False, only_selected_papers: Optional[bool] = False, only_removed_papers: Optional[bool] = False,
-           read_only: Optional[bool] = False, verbose: Optional[bool] = False):
+def refine(
+    search_path: str,
+    categories: Optional[dict] = None,
+    highlights: Optional[list] = None,
+    show_abstract: Optional[bool] = False,
+    show_extra_info: Optional[bool] = False,
+    only_selected_papers: Optional[bool] = False,
+    only_removed_papers: Optional[bool] = False,
+    read_only: Optional[bool] = False,
+    verbose: Optional[bool] = False,
+):
     """
     When you have a search result and wanna refine it, this is the method that you'll need to call.
-    This method will iterate through all the papers showing their collected data, 
+    This method will iterate through all the papers showing their collected data,
     then asking if you wanna select a particular paper or not, and assign a category if a list of categories is provided.
-    And to help you on the refinement, this method can also highlight some terms on the paper's abstract by a provided list of them 
+    And to help you on the refinement, this method can also highlight some terms on the paper's abstract by a provided list of them
 
     Parameters
     ----------
@@ -187,8 +237,6 @@ def refine(search_path: str, categories: Optional[dict] = None, highlights: Opti
     common_util.logging_initialize(verbose)
     common_util.check_write_access(search_path)
 
-    init(autoreset=True)  # colorama initializer
-
     if categories is None:
         categories = {}
     if highlights is None:
@@ -201,14 +249,17 @@ def refine(search_path: str, categories: Optional[dict] = None, highlights: Opti
         if paper.selected is not None:
             has_already_refined_papers = True
             break
-    
+
     todo_papers = []
     done_papers = []
 
     for paper in search.papers:
-        #if wanna_re_refine_papers:
-        if (only_selected_papers or only_removed_papers):
-            if paper.selected is not None and ((only_selected_papers and paper.selected) or (only_removed_papers and not paper.selected)):
+        # if wanna_re_refine_papers:
+        if only_selected_papers or only_removed_papers:
+            if paper.selected is not None and (
+                (only_selected_papers and paper.selected)
+                or (only_removed_papers and not paper.selected)
+            ):
                 todo_papers.append(paper)
         else:
             if paper.selected is None or read_only:
@@ -219,16 +270,14 @@ def refine(search_path: str, categories: Optional[dict] = None, highlights: Opti
     todo_papers = sorted(todo_papers, key=lambda x: x.publication_date, reverse=True)
 
     for i, paper in enumerate(todo_papers):
-        
         print(f"\n{'.' * os.get_terminal_size()[0]}\n")
 
         if not read_only:
-            print(f"\n{Fore.CYAN}{i+1}/{len(todo_papers)} papers\n")
+            print(f"\n{colored(f'{i + 1}/{len(todo_papers)} papers', 'cyan')}\n")
 
         _print_paper_details(paper, highlights, show_abstract, show_extra_info)
 
         if not read_only:
-
             answer = _get_select_question_input()
 
             if answer == "Skip":
@@ -242,10 +291,10 @@ def refine(search_path: str, categories: Optional[dict] = None, highlights: Opti
 
             if paper.selected:
                 paper.categories = _get_category_question_input(categories)
-            
+
             done_papers.append(paper)
 
     if read_only:
-        print(f"\n{Fore.CYAN}{len(todo_papers)} papers\n")
+        print(f"\n{colored(f'{len(todo_papers)} papers', 'cyan')}\n")
     else:
         persistence_util.save(search, search_path)
